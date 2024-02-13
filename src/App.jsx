322f0be73ae68a7f6e7a6d7900e517f2e2557092
Cizen14 <cizenmah@gmail.com>
2024-02-13 19:43:07 +1100

    started with the app and added the firebase

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+import SignUp from './Authentication/SignUp'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+<SignUp />
+    </>
+  )
+}
+
+export default App
