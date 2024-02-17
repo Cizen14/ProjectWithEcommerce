225743501e01825dacee74acaa714ac23212d7ae
Cizen14 <cizenmah@gmail.com>
2024-02-17 11:29:30 +1100

    done with the login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,22 @@ import { useState } from 'react'
 
 import './App.css'
 import SignUp from './Authentication/SignUp'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Login from './Authentication/Login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-<SignUp />
+    <BrowserRouter>
+    <Routes>
+      <Route path='/login' element={<Login/>} > </Route>
+      <Route path='/signup' element={<SignUp/>} > </Route>
+    </Routes>
+    </BrowserRouter>
+
+
     </>
   )
 }
